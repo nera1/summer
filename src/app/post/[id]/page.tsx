e782ca81c5adbb6484b3cc9d834026addbed75bf
nera1 <rustydoor4728@gmail.com>
2024-05-14 15:27:20 +0900

    render test

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { remark } from "remark";
+import html from "remark-html";
 import db from "@/app/db.json";
 import { readFileSync } from "fs";
 import { join } from "path";
@@ -6,12 +8,12 @@ export function generateStaticParams() {
   return db.list.map((item) => ({ id: item }));
 }
 
-export default function Page({ params }: { params: { id: string } }) {
-  // const value = await remark().use(html).process(data);
+export default async function Page({ params }: { params: { id: string } }) {
   const { dictionary }: any = db;
   const file = readFileSync(
     join(process.cwd(), "src", "md", dictionary[params.id]?.filename)
   ).toString();
+  const value = await remark().use(html).process(file);
   return (
     <main>
       <div>{file}</div>
