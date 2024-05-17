17406282c255e8d4c55809a78e700f18a2a55699
nera1 <rustydoor4728@gmail.com>
2024-05-17 15:46:41 +0900

    path test

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    path: "",
-  },
+  assetPrefix: "",
+  basePath: "",
 };
 
 export default nextConfig;
