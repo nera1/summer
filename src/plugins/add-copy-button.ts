8f6c3f82d520adcd1874ef054f00f7def1264266
nera1 <rustydoor4728@gmail.com>
2024-06-03 00:56:52 +0900

    remove codeValue attr from tag

--- src/plugins/add-copy-button.ts
+++ src/plugins/add-copy-button.ts
@@ -10,6 +10,7 @@ function AddCopyButton() {
         );
         if (preTag && node.properties && node.properties.codeValue) {
           const codeValue = node.properties.codeValue;
+          node.properties.codeValue = undefined;
           const button = h(
             "button",
             {
@@ -28,8 +29,8 @@ function AddCopyButton() {
               h(
                 "svg",
                 {
-                  width: "24",
-                  height: "24",
+                  width: "18",
+                  height: "18",
                   viewBox: "0 0 24 24",
                   fill: "none",
                   xmlns: "http://www.w3.org/2000/svg",
@@ -37,13 +38,13 @@ function AddCopyButton() {
                 [
                   h("path", {
                     d: "M5 9L5 19H15H17C17 20.1046 16.1046 21 15 21H5C3.89543 21 3 20.1046 3 19V9C3 7.89543 3.89543 7 5 7V9Z",
-                    fill: "#3c3c41",
+                    fill: "#dadada",
                   }),
                   h("path", {
                     fillRule: "evenodd",
                     clipRule: "evenodd",
                     d: "M19 5H9L9 15H19V5ZM9 3C7.89543 3 7 3.89543 7 5V15C7 16.1046 7.89543 17 9 17H19C20.1046 17 21 16.1046 21 15V5C21 3.89543 20.1046 3 19 3H9Z",
-                    fill: "#3c3c41",
+                    fill: "#dadada",
                   }),
                 ]
               ),
